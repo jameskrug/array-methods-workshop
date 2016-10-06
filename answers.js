ae96e465dd52f1a0dfd382d7328779d4ac77381f
James <krugger322@hotmail.com>
2016-10-06 17:53:23 +0000

    added positive number getter using filter function

--- answers.js
+++ answers.js
@@ -1,3 +1,4 @@
+/*
 function isPositive(num){
 
     if (num >= 0){
@@ -11,4 +12,22 @@ function printPositives(nums){
 var positiveNums = [];
 printPositives([1,-1,2,3,-3,4,5,6,7,-4,-56,8,112,3323,54,2343,76,-2,-3,-56]);
 console.log(positiveNums);
+
+*/
+
+
+function filterPositives(num){
+    return num >= 0;
+}
+
+function getPositives(nums){
+    
+    //filteredResults.push(nums.filter(filterPositives));
+    filteredResults = nums.filter(filterPositives);
+}
+
+var filteredResults = [];
+getPositives([1,-1,2,3,-3,4,5,6,7,-4,-56,8,112,3323,54,2343,76,-2,-3,-56]);
+console.log(filteredResults);
+
 
